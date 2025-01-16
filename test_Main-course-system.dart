be35fbf0262e405b1302988f685e9ed33b919cc2
Abrar khatib Lajim <88080003+AbrarBb@users.noreply.github.com>
2025-01-16 23:15:09 +0600

    Update and rename futuristic-course-system.dart to test_Main-course-system.dart

--- test_Main-course-system.dart
+++ test_Main-course-system.dart
@@ -9,8 +9,8 @@ class DatabaseConfig {
   static final settings = ConnectionSettings(
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'password',
+    user: 'lajim',
+    password: '1234',
     db: 'course_recommendation'
   );
 }
